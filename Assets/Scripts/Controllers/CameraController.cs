6fb9343747203166c175d5721ca7d60786221419
Schwitalle <maximilian.schwitalle@tu-clausthal.de>
2018-10-02 16:43:28 +0200

    einfache AI verfolgt Character

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
 
     public GameObject player;
+    public float x = 5;
+    public float y = -10;
+    public float z = 5; 
 
     private Vector3 offset;
 
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = player.transform.position - new Vector3(5, -10, 5);
     }
 
     void LateUpdate()
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,60 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour {
+    
+    public float movementSpeed = 5f;
 
-
-    public float rotationDamping = 20f;
-    public float runSpeed = 10f;
-    public int gravity = 20;
-    public float jumpSpeed = 20;
-
-    private bool canJump;
-    private float moveSpeed;
-    private float verticalVel;  // Used for continuing momentum while in air    
-    private CharacterController controller;
-
-    void Start()
+    private void Update()
     {
-        controller = (CharacterController)GetComponent(typeof(CharacterController));
-    }
-    float UpdateMovement()
-    {
-        // Movement
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 inputVec = new Vector3(x, 0, z);
-        inputVec *= runSpeed;
-        controller.Move((inputVec + Vector3.up * -gravity + new Vector3(0, verticalVel, 0)) * Time.deltaTime);
-        // Rotation
-        if (inputVec != Vector3.zero)
-            transform.rotation = Quaternion.Slerp(transform.rotation,
-                Quaternion.LookRotation(inputVec),
-                Time.deltaTime * rotationDamping);
-        return inputVec.magnitude;
-    }
-    void Update()
-    {
-        // Check for jump
-        if (controller.isGrounded)
-        {
-            canJump = true;
-            if (canJump && Input.GetKeyDown("space"))
-            {
-                // Apply the current movement to launch velocity
-                verticalVel = jumpSpeed;
-                canJump = false;
-            }
-        }
-        else
-        {
-            // Apply gravity to our velocity to diminish it over time
-            verticalVel += Physics.gravity.y * Time.deltaTime;
-        }
-        // Actually move the character
-        moveSpeed = UpdateMovement();
-        if (controller.isGrounded)
-            verticalVel = 0f;// Remove any persistent velocity after landing
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement != Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), Time.deltaTime * 5f);
+
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
     }
 }
